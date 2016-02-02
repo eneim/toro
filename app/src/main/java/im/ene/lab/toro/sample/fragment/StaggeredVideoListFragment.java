53c40ab21745b24dbc46c461d13e6b16720adc35
Nguyen Hoai Nam <mr.nguyenhoainam@gmail.com>
2016-02-02 10:07:08 +0900

    Try reversed layout

--- app/src/main/java/im/ene/lab/toro/sample/fragment/StaggeredVideoListFragment.java
+++ app/src/main/java/im/ene/lab/toro/sample/fragment/StaggeredVideoListFragment.java
@@ -18,8 +18,10 @@ package im.ene.lab.toro.sample.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
 import im.ene.lab.toro.sample.R;
 import im.ene.lab.toro.sample.adapter.SampleVideoUrlListAdapter;
 
@@ -33,6 +35,11 @@ public class StaggeredVideoListFragment extends RecyclerViewFragment {
     StaggeredVideoListFragment fragment = new StaggeredVideoListFragment();
     fragment.setArguments(args);
     return fragment;
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    // mRecyclerView.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
   }
 
   @NonNull @Override protected RecyclerView.LayoutManager getLayoutManager() {
