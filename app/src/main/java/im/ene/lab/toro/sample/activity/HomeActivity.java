45b6ed3b95a0eb19f2aed306a1b78eca19498664
Nguyen Hoai Nam <mr.nguyenhoainam@gmail.com>
2016-02-06 11:57:29 +0900

    Update strategy selector

--- app/src/main/java/im/ene/lab/toro/sample/activity/HomeActivity.java
+++ app/src/main/java/im/ene/lab/toro/sample/activity/HomeActivity.java
@@ -18,20 +18,44 @@ package im.ene.lab.toro.sample.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import im.ene.lab.toro.Toro;
+import im.ene.lab.toro.ToroStrategy;
 import im.ene.lab.toro.sample.R;
 import im.ene.lab.toro.sample.fragment.DualVideoListFragment;
 import im.ene.lab.toro.sample.fragment.MultiVideoComplicatedGridFragment;
 import im.ene.lab.toro.sample.fragment.MultiVideoStaggeredGridFragment;
 import im.ene.lab.toro.sample.fragment.SingleVideoSimpleListFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+  @Bind(R.id.strategies_container) RadioGroup mStrategies;
+
+  private final ToroStrategy[] STRATEGIES = {
+      Toro.Strategies.FIRST_PLAYABLE_TOP_DOWN, Toro.Strategies.FIRST_PLAYABLE_TOP_DOWN_KEEP_LAST,
+      Toro.Strategies.MOST_VISIBLE_TOP_DOWN, Toro.Strategies.MOST_VISIBLE_TOP_DOWN_KEEP_LAST
+  };
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
     ButterKnife.bind(this);
+
+    mStrategies.setOnCheckedChangeListener(this);
+    mStrategies.removeAllViews();
+
+    for (ToroStrategy strategy : STRATEGIES) {
+      RadioButton checkBox = (RadioButton) LayoutInflater.from(mStrategies.getContext())
+          .inflate(R.layout.strategy_checkbox, mStrategies, false);
+      checkBox.setText(strategy.getDescription());
+      mStrategies.addView(checkBox);
+      checkBox.setChecked(Toro.getStrategy() == strategy);
+    }
   }
 
   @OnClick(R.id.btn_single_video_simple_list) void singleVideoSimpleList() {
@@ -48,5 +72,18 @@ public class HomeActivity extends AppCompatActivity {
 
   @OnClick(R.id.btn_multi_video_dual_list) void multiVideoDualList() {
     startActivity(ShowCaseActivity.createIntent(this, DualVideoListFragment.TAG));
+  }
+
+  @Override public void onCheckedChanged(RadioGroup group, int checkedId) {
+    RadioButton buttonView = (RadioButton) group.findViewById(checkedId);
+    if (buttonView != null) {
+      String policyName = buttonView.getText() + "";
+      for (ToroStrategy policy : STRATEGIES) {
+        if (policy.getDescription().equals(policyName)) {
+          Toro.setStrategy(policy);
+          break;
+        }
+      }
+    }
   }
 }
