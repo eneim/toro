262df64236583b2930ab8ce0271fa1c7945673d8
eneim <mr.nguyenhoainam@gmail.com>
2017-06-29 00:13:21 +0900

    revise the player view helpers

--- app/src/main/java/im/ene/toro/sample/legacy/LegacyVideoViewHelper.java
+++ app/src/main/java/im/ene/toro/sample/legacy/LegacyVideoViewHelper.java
@@ -14,28 +14,35 @@
  * limitations under the License.
  */
 
-package im.ene.toro.helper;
+package im.ene.toro.sample.legacy;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import com.google.android.exoplayer2.C;
 import im.ene.toro.ToroPlayer;
 import im.ene.toro.ToroPlayer.State;
+import im.ene.toro.helper.ToroPlayerHelper;
 import im.ene.toro.media.PlaybackInfo;
 import im.ene.toro.widget.Container;
-import im.ene.toro.widget.ToroVideoView;
 
 import static android.media.MediaPlayer.MEDIA_INFO_BUFFERING_END;
 import static android.media.MediaPlayer.MEDIA_INFO_BUFFERING_START;
 
 /**
  * @author eneim | 6/11/17.
+ *
+ *         Helper class for {@link ToroVideoView}. This class makes the playback using {@link
+ *         ToroVideoView} easier.
  */
+
 @SuppressWarnings({ "WeakerAccess", "ConstantConditions" }) //
 public class LegacyVideoViewHelper extends ToroPlayerHelper {
 
+  final PlaybackInfo playbackInfo = new PlaybackInfo();
   @NonNull final ToroVideoView playerView;
   @NonNull final Uri mediaUri;
 
@@ -44,7 +51,7 @@ public class LegacyVideoViewHelper extends ToroPlayerHelper {
   MediaPlayer.OnPreparedListener onPreparedListener;
 
   @State int playerState = State.STATE_IDLE;
-  boolean playWhenReady = false;
+  boolean playWhenReady = false;  // mimic the ExoPlayer
 
   public LegacyVideoViewHelper(Container container, ToroPlayer player, @NonNull Uri mediaUri) {
     super(container, player);
@@ -66,61 +73,57 @@ public class LegacyVideoViewHelper extends ToroPlayerHelper {
     this.onPreparedListener = onPreparedListener;
   }
 
-  @Override public void initialize(@NonNull final PlaybackInfo playbackInfo) {
-    this.playerView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-      @Override public void onCompletion(MediaPlayer mp) {
-        playerState = State.STATE_END;
-        onPlayerStateUpdated(playWhenReady, playerState);
-        if (LegacyVideoViewHelper.this.onCompletionListener != null) {
-          LegacyVideoViewHelper.this.onCompletionListener.onCompletion(mp);
-        }
-        // Reset the player so it can be immediately reused.
-        if (mediaPlayer != null) {
-          mediaPlayer.reset();
-        }
-        playWhenReady = false;  // !!Keeping playWhenReady as true will make a loop playback.
-        playerView.setVideoURI(mediaUri);
-      }
-    });
-    this.playerView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-      @Override public void onPrepared(MediaPlayer mp) {
-        playerState = State.STATE_READY;
-        onPlayerStateUpdated(playWhenReady, playerState);
+  @Override public void initialize(@Nullable final PlaybackInfo playbackInfo) {
+    if (playbackInfo != null) {
+      this.playbackInfo.setResumePosition(playbackInfo.getResumePosition());
+    }
 
-        mediaPlayer = mp;
-        if (LegacyVideoViewHelper.this.onPreparedListener != null) {
-          LegacyVideoViewHelper.this.onPreparedListener.onPrepared(mp);
-        }
-        if (playWhenReady) play();
+    this.playerView.setOnCompletionListener(mp -> {
+      playerState = State.STATE_END;
+      onPlayerStateUpdated(playWhenReady, playerState);
+      if (LegacyVideoViewHelper.this.onCompletionListener != null) {
+        LegacyVideoViewHelper.this.onCompletionListener.onCompletion(mp);
       }
+      // Reset the player so it can be immediately reused.
+      if (mediaPlayer != null) {
+        mediaPlayer.reset();
+      }
+      playWhenReady = false;  // !!Keeping playWhenReady as true will make a loop playback.
+      playerView.setVideoURI(mediaUri);
+    });
+    this.playerView.setOnPreparedListener(mp -> {
+      playerState = State.STATE_READY;
+      onPlayerStateUpdated(playWhenReady, playerState);
+
+      mediaPlayer = mp;
+      if (LegacyVideoViewHelper.this.onPreparedListener != null) {
+        LegacyVideoViewHelper.this.onPreparedListener.onPrepared(mp);
+      }
+      if (playWhenReady) play();
     });
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      this.playerView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-        @Override public boolean onInfo(MediaPlayer mp, int what, int extra) {
-          boolean handled;
-          switch (what) {
-            case MEDIA_INFO_BUFFERING_START:
-              playerState = State.STATE_BUFFERING;
-              onPlayerStateUpdated(playWhenReady, playerState);
-              handled = true;
-              break;
-            case MEDIA_INFO_BUFFERING_END:
-              playerState = State.STATE_READY;
-              onPlayerStateUpdated(playWhenReady, playerState);
-              handled = true;
-              break;
-            default:
-              handled = false;
-              break;
-          }
-          return handled;
+      this.playerView.setOnInfoListener((mp, what, extra) -> {
+        boolean handled;
+        switch (what) {
+          case MEDIA_INFO_BUFFERING_START:
+            playerState = State.STATE_BUFFERING;
+            onPlayerStateUpdated(playWhenReady, playerState);
+            handled = true;
+            break;
+          case MEDIA_INFO_BUFFERING_END:
+            playerState = State.STATE_READY;
+            onPlayerStateUpdated(playWhenReady, playerState);
+            handled = true;
+            break;
+          default:
+            handled = false;
+            break;
         }
+        return handled;
       });
     }
-    this.playerView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-      @Override public boolean onError(MediaPlayer mp, int what, int extra) {
-        return true;  // to prevent the system error dialog.
-      }
+    this.playerView.setOnErrorListener((mp, what, extra) -> {
+      return true;  // to prevent the system error dialog.
     });
     this.playerView.setPlayerEventListener(new ToroVideoView.PlayerEventListener() {
       @Override public void onPlay() {
@@ -135,32 +138,39 @@ public class LegacyVideoViewHelper extends ToroPlayerHelper {
     });
 
     this.playerView.setVideoURI(mediaUri);
-    if (playbackInfo.getResumePosition() >= 0) {
-      this.playerView.seekTo((int) playbackInfo.getResumePosition());
+    if (this.playbackInfo.getResumePosition() >= 0) {
+      this.playerView.seekTo((int) this.playbackInfo.getResumePosition());
     }
   }
 
-  public void play() {
+  @Override public void play() {
+    Log.w(TAG, "play: " + this.playbackInfo.getResumePosition());
     this.playerView.start();
   }
 
-  public void pause() {
+  @Override public void pause() {
+    updateResumePosition();
     this.playerView.pause();
   }
 
-  public boolean isPlaying() {
+  @Override public boolean isPlaying() {
     return playWhenReady || this.playerView.isPlaying();
   }
 
-  @Override public PlaybackInfo updatePlaybackInfo() {
-    int position = 0;
+  @NonNull @Override public PlaybackInfo getLatestPlaybackInfo() {
+    updateResumePosition();
+    return new PlaybackInfo(C.INDEX_UNSET, playbackInfo.getResumePosition());
+  }
+
+  void updateResumePosition() {
+    boolean success = false;
     try {
-      position = mediaPlayer != null ? mediaPlayer.getCurrentPosition() : C.POSITION_UNSET;
+      if (mediaPlayer != null) playbackInfo.setResumePosition(mediaPlayer.getCurrentPosition());
+      success = true;
     } catch (IllegalStateException er) {
       er.printStackTrace();
     }
-
-    return new PlaybackInfo(C.INDEX_UNSET, position);
+    Log.i(TAG, "updateResumePosition: " + success);
   }
 
   @Override public void cancel() throws Exception {
