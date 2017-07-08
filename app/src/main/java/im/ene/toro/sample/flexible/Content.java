b019e3a1b53429ccc6090c8d83b66257d72c8f48
eneim <mr.nguyenhoainam@gmail.com>
2017-07-08 18:53:36 +0900

    minor improvement

--- app/src/main/java/im/ene/toro/sample/flexible/Content.java
+++ app/src/main/java/im/ene/toro/sample/flexible/Content.java
@@ -22,21 +22,25 @@ import android.net.Uri;
  * @author eneim (7/1/17).
  */
 
-class Content {
+public class Content {
 
-  static final String MP4_BUNNY = "file:///android_asset/bbb.mp4";
+  private static final String MP4_BUNNY = "file:///android_asset/bbb.mp4";
+  private static final String MP4_TOS = "file:///android_asset/tos.mp4";
+  private static final String MP4_COSMOS = "file:///android_asset/cosmos.mp4";
+
+  static final String[] ITEMS = { MP4_TOS, MP4_BUNNY, MP4_COSMOS };
 
   public static class Media {
-    final int index;
+    public final int index;
     public final Uri mediaUri;
 
-    Media(int index, Uri mediaUri) {
+    public Media(int index, Uri mediaUri) {
       this.index = index;
       this.mediaUri = mediaUri;
     }
 
     static Media getItem(int index) {
-      return new Media(index, Uri.parse(MP4_BUNNY));
+      return new Media(index, Uri.parse(ITEMS[index % ITEMS.length]));
     }
 
     @Override public boolean equals(Object o) {
