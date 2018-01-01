222405570cbffaa971ddef7eea58977afeb970a6
eneim <mr.nguyenhoainam@gmail.com>
2018-01-01 22:16:18 +0900

    Update changelog

--- toro-mopub/src/main/java/im/ene/toro/exoplayer/DrmMediaProvider.java
+++ toro-mopub/src/main/java/im/ene/toro/exoplayer/DrmMediaProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Nam Nguyen, nam@ene.im
+ * Copyright (c) 2018 Nam Nguyen, nam@ene.im
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
