f751d9e0108486929f519a485f5742d1f2643f84
eneim <mr.nguyenhoainam@gmail.com>
2018-02-19 21:47:29 +0900

    minor code cleanup

--- demo-exoplayer/src/main/java/toro/demo/exoplayer/basic/VideoViewHolder.kt
+++ demo-exoplayer/src/main/java/toro/demo/exoplayer/basic/VideoViewHolder.kt
@@ -17,13 +17,11 @@
 package toro.demo.exoplayer.basic
 
 import android.net.Uri
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
 import im.ene.toro.ToroPlayer
-import im.ene.toro.ToroPlayer.EventListener
 import im.ene.toro.ToroUtil.visibleAreaOffset
 import im.ene.toro.exoplayer.ExoPlayerViewHelper
 import im.ene.toro.media.PlaybackInfo
@@ -43,31 +41,12 @@ internal class VideoViewHolder(inflater: LayoutInflater?, parent: ViewGroup?) :
     companion object {
         val regex = Pattern.compile("(\\d)+\\.(\\d+)")!!
         const val defaultRatio = 100 * 165.78F / 360F
-        const val TAG = "ToroExo:Basic"
     }
 
     private val playerFrame by lazy { itemView as AspectRatioFrameLayout }
     private val player = itemView.findViewById(R.id.player) as SimpleExoPlayerView
     private var helper: ExoPlayerViewHelper? = null
     private var videoUri: Uri? = null
-
-    private val listener = object: EventListener {
-        override fun onBuffering() {
-            Log.w(TAG, "onBuffering")
-        }
-
-        override fun onPlaying() {
-            Log.d(TAG, "onPlaying")
-        }
-
-        override fun onPaused() {
-            Log.d(TAG, "onPaused")
-        }
-
-        override fun onCompleted() {
-            Log.d(TAG, "onCompleted")
-        }
-    }
 
     override fun bind(item: Any?) {
         super.bind(item)
@@ -90,25 +69,21 @@ internal class VideoViewHolder(inflater: LayoutInflater?, parent: ViewGroup?) :
         if (helper === null) {
             helper = ExoPlayerViewHelper(container, this,
                     videoUri!!, DemoApp.exoCreator!!, null)
-            helper!!.addPlayerEventListener(listener)
         }
         helper!!.initialize(playbackInfo)
     }
 
     override fun play() {
-        Log.d(TAG, hashCode().toString() + "#play()")
         helper!!.play()
     }
 
     override fun pause() {
-        Log.d(TAG, hashCode().toString() + "#pause()")
         helper!!.pause()
     }
 
     override fun isPlaying() = helper?.isPlaying ?: false
 
     override fun release() {
-        helper?.removePlayerEventListener(listener)
         helper?.release()
         helper = null
     }
