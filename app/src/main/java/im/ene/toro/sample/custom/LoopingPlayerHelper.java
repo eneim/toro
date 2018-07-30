c8cf471f3a75bd1afd9d0dfe8b85ab560ee92c41
eneim <mr.nguyenhoainam@gmail.com>
2018-07-30 20:59:22 +0900

    dependency version up

--- app/src/main/java/im/ene/toro/sample/custom/LoopingPlayerHelper.java
+++ app/src/main/java/im/ene/toro/sample/custom/LoopingPlayerHelper.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import im.ene.toro.ToroPlayer;
 import im.ene.toro.exoplayer.Config;
+import im.ene.toro.exoplayer.ExoPlayable;
 import im.ene.toro.exoplayer.ExoPlayerViewHelper;
 import im.ene.toro.exoplayer.MediaSourceBuilder;
 
@@ -27,17 +28,23 @@ import im.ene.toro.exoplayer.MediaSourceBuilder;
  * @author eneim (2018/01/05).
  */
 
-class LoopingPlayerHelper extends ExoPlayerViewHelper {
+@SuppressWarnings("unused") class LoopingPlayerHelper extends ExoPlayerViewHelper {
 
   private static Config loopingConfig =
       new Config.Builder().setMediaSourceBuilder(MediaSourceBuilder.LOOPING).build();
 
+  @SuppressWarnings("unused") //
   LoopingPlayerHelper(@NonNull ToroPlayer player, @NonNull Uri mediaUri) {
     // customized using Looping media source builder
     this(player, mediaUri, null);
   }
 
+  @SuppressWarnings("WeakerAccess") //
   LoopingPlayerHelper(@NonNull ToroPlayer player, @NonNull Uri mediaUri, String extension) {
     super(player, mediaUri, extension, loopingConfig);
+  }
+
+  LoopingPlayerHelper(@NonNull ToroPlayer player, @NonNull ExoPlayable playable) {
+    super(player, playable);
   }
 }
