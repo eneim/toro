3b49930935e0b185a77ccd125ff6c899cef4390f
eneim <mr.nguyenhoainam@gmail.com>
2018-08-25 20:46:32 +0900

    make AdsPlayable public

--- toro-exoplayer/src/main/java/im/ene/toro/exoplayer/AdsPlayable.java
+++ toro-exoplayer/src/main/java/im/ene/toro/exoplayer/AdsPlayable.java
@@ -17,6 +17,7 @@
 package im.ene.toro.exoplayer;
 
 import android.net.Uri;
+import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -30,13 +31,13 @@ import im.ene.toro.ToroPlayer;
 import im.ene.toro.annotations.Beta;
 
 /**
- * Only use this inside a {@link AdsExoPlayerViewHelper}.
+ * A {@link Playable} that is able to integrate with {@link AdsLoader}.
  *
  * @author eneim (2018/08/22).
  * @since 3.6.0.2802
  */
 @Beta //
-class AdsPlayable extends ExoPlayable {
+public class AdsPlayable extends ExoPlayable {
 
   static class FactoryImpl implements AdsMediaSource.MediaSourceFactory {
 
@@ -62,7 +63,8 @@ class AdsPlayable extends ExoPlayable {
   @NonNull private final FactoryImpl factory;
   @Nullable private final ViewGroup adsContainer;
 
-  /* package */ AdsPlayable(ExoCreator creator, Uri uri, String fileExt, ToroPlayer player,
+  @SuppressWarnings("WeakerAccess")
+  public AdsPlayable(ExoCreator creator, Uri uri, String fileExt, ToroPlayer player,
       @NonNull AdsLoader adsLoader, @Nullable ViewGroup adsContainer) {
     super(creator, uri, fileExt);
     this.adsLoader = adsLoader;
@@ -70,6 +72,7 @@ class AdsPlayable extends ExoPlayable {
     this.factory = new FactoryImpl(this.creator, player);
   }
 
+  @CallSuper
   @Override public void prepare(boolean prepareSource) {
     this.mediaSource = createAdsMediaSource(creator, mediaUri, fileExt, //
         factory.player, adsLoader, adsContainer, factory);
